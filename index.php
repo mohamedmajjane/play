69f6328c23f78ecf50547fa6cd537c69c1908b4f
mohamedmajjane <mohamed@majjane.com>
2015-11-11 16:08:10 +0100

    ajout d'une paragraphe
    tag p

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 		<title>Document</title>
 	</head>
 	<body>
-		
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero nostrum minima ipsam reiciendis. Veritatis fugit tempora libero dolorum, delectus aspernatur.
+		</p>
 	</body>
 	</html>	
